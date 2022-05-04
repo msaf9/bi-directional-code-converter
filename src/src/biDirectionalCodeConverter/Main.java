63a02a2c23b54221d4532a1ac4ee058f76fe44a7
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-05-04 15:22:40 +0530

    Added input

--- src/src/biDirectionalCodeConverter/Main.java
+++ src/src/biDirectionalCodeConverter/Main.java
@@ -1,16 +1,23 @@
 package biDirectionalCodeConverter;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String args[]) {
 
-		for (int i = 0; i < 16; i++) {
-			int binary = DecimalToBinary.toBinary(i);
-			int grey = BinaryToGrey.toGrey(i);
+		int decimal, binary, grey;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your decimal number: ");
+		decimal = sc.nextInt();
+		sc.close();
 
-			System.out.print("The binary code of the decimal number " + i + " is ");
-			System.out.println(String.format("%04d", binary).substring(0));
-			System.out.print("The grey code of the decimal number " + i + " is ");
-			System.out.println(String.format("%04d", grey).substring(0));
-		}
+		binary = DecimalToBinary.toBinary(decimal);
+		grey = BinaryToGrey.toGrey(decimal);
+
+		System.out.print("The binary code of the decimal number " + decimal + " is ");
+		System.out.println(String.format("%04d", binary).substring(0));
+		System.out.print("The grey code of the decimal number " + decimal + " is ");
+		System.out.println(String.format("%04d", grey).substring(0));
+
 	}
 }
